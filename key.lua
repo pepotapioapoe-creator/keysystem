435adfb7c60e21f197a93f8b3c133bba47159708
pepotapioapoe-creator <pepotapioapoe@gmail.com>
2026-09-17 13:39:48 -0300

    Update key.lua

--- key.lua
+++ key.lua
@@ -44,8 +44,7 @@ local Config = {
 
     -- [7] Hub Information & UI Text
     HubName         = "Zvolt hub", -- The main title shown at the top of the GUI
-    HubDescription  = "
-The best FREE script" -- The text shown below the title
+    HubDescription  = "The best FREE script" -- The text shown below the title
 }
 
 -------------------------------------------------------------------------------
